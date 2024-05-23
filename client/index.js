373731c1b04bbee728f55446d8a570bcb246e392
JadeLaz <160426101+JadeLaz@users.noreply.github.com>
2024-05-23 05:27:41 +0000

    ⭐Feat: AirBnB

--- client/index.js
+++ client/index.js
@@ -1,25 +1,3 @@
-import './styles/style.css'
+import './styles/style.css';
 
-
-console.log("🎉 Client Server Working powered by webpack");
-
-let show = (msg="No message given") => {
-    console.log(msg)
-}
-
-function resolveAfter25Seconds() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('resolved');
-        }, 2000)
-    });
-}
-
-async function asyncCall() {
-    console.log("Calling");
-    const result = await resolveAfter25Seconds();
-    console.log(result);
-}
-
-asyncCall();
-show();
+console.log('🎉 Client Server Working powered by webpack');
